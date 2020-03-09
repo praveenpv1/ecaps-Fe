094002a2bb9da2ee2e0cc6cf74d4b9eb93b7b88f
Preshin PS <preshingithub@gmail.com>
2020-03-09 17:25:31 +0530

    clonned koppr-empolyer-front-end

---
+++ src/app/core/services/mock.service.ts
@@ -0,0 +1,40 @@
+export const mockData = {
+  spend: {
+    spendsData: {
+      data: {
+        transactions: {
+          totalAmount: 0,
+          categories: [
+            {
+              type: "Shopping",
+              amount: 4899
+            },
+            {
+              type: "Travel",
+              amount: 13540
+            },
+            {
+              type: "Entertainment",
+              amount: 680
+            },
+            {
+              type: "Food",
+              amount: 1575
+            },
+            {
+              type: "ATM Money",
+              amount: 3000
+            }
+          ]
+        }
+      }
+    }
+  },
+  pot: {
+    createData: {
+      data: {
+        _id: 10001
+      }
+    }
+  }
+};
