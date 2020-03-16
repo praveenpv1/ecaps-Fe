672a63cb22dbe2f504d27a7795e6b794fe75fedd
shyam <shyamprasadc1995@gmail.com>
2020-03-16 17:10:38 +0530

    update

--- src/app/core/components/retailer/add-retailer.component.ts
+++ src/app/core/components/retailer/add-retailer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-addnewuser',
@@ -6,27 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-retailer.component.scss']
 })
 export class AddRetailerComponent implements OnInit {
+  validateForm: FormGroup; 
 
-    isVisible = false;
-    isConfirmLoading = false;
-  constructor() { }
-
-  showModal(): void {
-    this.isVisible = true;
+  submitForm() {
+    for (const i in this.validateForm.controls) {
+      this.validateForm.controls[i].markAsDirty();
+      this.validateForm.controls[i].updateValueAndValidity();
+    }
   }
+  constructor(private fb: FormBuilder) { }
 
-  handleOk(): void {
-    this.isConfirmLoading = true;
-    setTimeout(() => {
-      this.isVisible = false;
-      this.isConfirmLoading = false;
-    }, 3000);
-  }
-
-  handleCancel(): void {
-    this.isVisible = false;
-  }
+   
   ngOnInit() {
+    this.validateForm = this.fb.group({
+      kitsAvailable: [null, [Validators.required]],
+      first_name: [null, [Validators.required]],
+      last_name: [null, [Validators.required]],
+      company_name: [null, [Validators.required]],
+      phoneNumberPrefix: ['+91'],
+      phoneNumber: [null, [Validators.required]],      
+      email: [null, [Validators.email, Validators.required]],
+      dateOfBirth: [null],
+      pan: [null, [Validators.required]],
+      aadhaarNo: [null, [Validators.required]],
+      voterId: [null, [Validators.required]],
+      
+    });
   }
 
 }
