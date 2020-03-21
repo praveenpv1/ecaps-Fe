775ec4cfe577f526910dbf0102ea70234abd7343
Ap Shuhaib <apshuhaib@gmail.com>
2020-03-21 13:19:19 +0530

    updates

--- src/app/core/components/ledgers/ledgers.component.ts
+++ src/app/core/components/ledgers/ledgers.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface LedgerList {
+  sNo:number;
+  timeStamp:string;
+  transType:string;
+  prevBal:number;
+  amount:number
+  afterBal:number;
+  opName:string;
+  custNo:number;
+}
+
 @Component({
   selector: 'app-ledgers',
   templateUrl: './ledgers.component.html',
   styleUrls: ['./ledgers.component.scss']
 })
-export class LedgersComponent implements OnInit { 
+export class LedgersComponent implements OnInit {
 
   constructor( ) { }
 
