2ef739dbe71ba6d06f80f62577be843b347ed50d
Shyam Prasad <shyamprasadc1995@gmail.com>
2020-03-23 12:23:38 +0530

    Revert "Updates3"

--- src/app/core/components/wallet-top-up/wallet-top-up.component.ts
+++ src/app/core/components/wallet-top-up/wallet-top-up.component.ts
@@ -33,12 +33,7 @@ import { environment } from "@env/environment";
     styleUrls: ["./wallet-top-up.component.scss"]
 })
 export class WalletTopUpComponent implements OnInit, OnDestroy {
-  Retailers: Retailer[] = [
-   {value: 'Retailer-0', viewValue: 'Retailer1'},
-   {value: 'Retailer-1', viewValue: 'Retailer2'},
-   {value: 'Retailer-2', viewValue: 'Retailer3'},
- ];
-    tabs = ["Transfer Value", "Transfer By Number","Request Value"];
+    tabs = ["Online Top Up", "Transactions"];
     validateForm: FormGroup;
     validateOnlineForm: FormGroup;
     selectedTransType = "neft";
